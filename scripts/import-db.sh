90d169443c78998bfb37a05ca4b0ef5860ff7520
Chris McIntosh <cmcintosh@wembassy.com>
2016-12-02 15:30:11 +0800

    fixing up scripts

--- scripts/import-db.sh
+++ scripts/import-db.sh
@@ -2,5 +2,7 @@
 
 # how to use: from root of docker boilerplate,
 # ./scripts/import-db SERVER_IP CONTAINER_NAME
+rm ./sql/mp.sql
 mysqldump -u root -p -h $1 mp > ./sql/mp.sql
-docker exec -i $2 mysql -uroot -proot meetingpackage < dump.sql
+docker exec -i $2 mysql -uroot -proot -e"DROP DATABASE meetingpackage; CREATE DATABASE meetingpackage;"
+docker exec -i $2 mysql -uroot -proot meetingpackage < ./sql/mp.sql
