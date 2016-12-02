2f287d9488e42f6a5a661a77a6b660e241f280bb
Chris McIntosh <cmcintosh@wembassy.com>
2016-12-02 15:21:59 +0800

    Adding instructions

--- scripts/import-db.sh
+++ scripts/import-db.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+# how to use: from root of docker boilerplate,
+# ./scripts/import-db SERVER_IP CONTAINER_NAME
 mysqldump -u root -p -h $1 mp > ./sql/mp.sql
 docker exec -i $2 mysql -uroot -proot meetingpackage < dump.sql
